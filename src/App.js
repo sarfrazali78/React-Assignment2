a5d8d78d7dc8dd6a8c42f95033dbc88318172a4d
sarfrazali78 <sarfrazalikamleu@gmail.com>
2023-04-26 23:26:56 +0530

    hyyy how r u

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [count, setCount]= useState(0);
+  function handleClick(){
+    setCount(count+1);
+  }
+   function handleClickto(){
+    if(count>0){
+    setCount(count-1);
+  }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App:</h1>
+      <h2>{count}</h2>
+      <button onClick={ handleClick}>Increase the count</button>
+      &nbsp;
+      &nbsp;
+      <button onClick={handleClickto}>Decrease the count</button>
     </div>
   );
-}
-
-export default App;
+  }
+  
